f74f81e0c5e90df1e13114e4769ceaff4f6b56e6
RUPESH GUPTA <87369931+rupeshgupta951@users.noreply.github.com>
2022-11-30 13:17:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+// USER-PROMPT FEATURE:
+
+//  console.log("Everything Is Fine");
+//  var user = prompt("Your Good Name Please?");
+//  if (user != null) {
+//    var text = document.getElementById("T5");
+//    text.style.color = "Blue";
+//    text.style.textTransform = "capitalize";
+//    text.innerHTML = `${user}` + "!";
+//  } else {
+//    alert("Looks so excited to visit my website");
+//    var text3 = document.getElementById("T5");
+//    text3.style.color = "Blue";
+//    text3.style.textTransform = "capitalize";
+//    text3.innerHTML = "Dude!";
+//  }
+//  const alertfunction = () => {
+//    alert("Looks so excited to visit my website");
+//    var text3 = document.getElementById("T5");
+//    text3.style.color = "Blue";
+//    text3.style.textTransform = "capitalize";
+//    text3.innerHTML = "Dude!";
+//    return text3;
+//  };
+//  if (user === "") {
+//    alertfunction();
+//  }
+
+// CONTACT US SUBMISSION:
